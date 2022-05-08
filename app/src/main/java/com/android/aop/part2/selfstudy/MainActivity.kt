a984f65e866687efa28d8a8557097ddbaaded900
Dustin_Jung <jadebejung@gmail.com>
2022-05-08 10:21:30 +0900

    init

---
+++ app/src/main/java/com/android/aop/part2/selfstudy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.aop.part2.selfstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
